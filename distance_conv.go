cab689e15c707ce8940b1d033168a7a439a5cb93
Chris Dzombak <chris@dzombak.com>
2023-09-02 14:01:42 -0400

    add meters

--- distance_conv.go
+++ distance_conv.go
@@ -10,6 +10,11 @@ func (mi Mile) NauticalMiles() NauticalMile {
 	return NauticalMile(mi / 1.15078)
 }
 
+// Meters returns the distance in meters.
+func (mi Mile) Meters() Meter {
+	return Meter(mi * 1609.34)
+}
+
 // Miles returns the distance in miles.
 func (km Km) Miles() Mile {
 	return Mile(km / 1.60934)
@@ -20,6 +25,11 @@ func (km Km) NauticalMiles() NauticalMile {
 	return NauticalMile(km / 1.852)
 }
 
+// Meters returns the distance in meters.
+func (km Km) Meters() Meter {
+	return Meter(km * 1000)
+}
+
 // Miles returns the distance in miles.
 func (nm NauticalMile) Miles() Mile {
 	return Mile(nm * 1.15078)
@@ -28,4 +38,24 @@ func (nm NauticalMile) Miles() Mile {
 // Km returns the distance in kilometers.
 func (nm NauticalMile) Km() Km {
 	return Km(nm * 1.852)
+}
+
+// Meters returns the distance in meters.
+func (nm NauticalMile) Meters() Meter {
+	return Meter(nm * 1852)
+}
+
+// Miles returns the distance in miles.
+func (m Meter) Miles() Mile {
+	return Mile(m / 1609.34)
+}
+
+// Km returns the distance in kilometers.
+func (m Meter) Km() Km {
+	return Km(m / 1000)
+}
+
+// NauticalMiles returns the distance in nautical miles.
+func (m Meter) NauticalMiles() NauticalMile {
+	return NauticalMile(m / 1852)
 }
--- distance_types.go
+++ distance_types.go
@@ -3,6 +3,9 @@ package libwx
 // Mile represents distance in miles.
 type Mile float64
 
+// Meter represents distance in meters.
+type Meter float64
+
 // Km represents distance in kilometers.
 type Km float64
 
@@ -10,5 +13,6 @@ type Km float64
 type NauticalMile float64
 
 func (mi Mile) Unwrap() float64         { return float64(mi) }
+func (m Meter) Unwrap() float64         { return float64(m) }
 func (km Km) Unwrap() float64           { return float64(km) }
 func (nm NauticalMile) Unwrap() float64 { return float64(nm) }
