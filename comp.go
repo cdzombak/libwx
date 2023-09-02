95dd18436a9036c1c6323bcfa109bee11613e9f7
Chris Dzombak <chris@dzombak.com>
2023-09-02 13:48:29 -0400

    add comparison utilities

---
+++ comp.go
@@ -0,0 +1,45 @@
+package libwx
+
+const (
+	ToleranceExact = float64(0.0)
+	Tolerance0     = float64(1.0)
+	Tolerace1      = float64(0.1)
+	Tolerance01    = float64(0.01)
+	Tolerance001   = float64(0.001)
+)
+
+func Float64Compare(a, b, tolerance float64) int {
+	if a > b+tolerance {
+		return 1
+	}
+	if a < b-tolerance {
+		return -1
+	}
+	return 0
+}
+
+func IntCompare(a, b int) int {
+	if a > b {
+		return 1
+	}
+	if a < b {
+		return -1
+	}
+	return 0
+}
+
+func Float64Equal(a, b, tolerance float64) bool {
+	return 0 == Float64Compare(a, b, tolerance)
+}
+
+func CurriedFloat64Compare(tolerance float64) func(float64, float64) int {
+	return func(a, b float64) int {
+		return Float64Compare(a, b, tolerance)
+	}
+}
+
+func CurriedFloat64Equal(tolerance float64) func(float64, float64) bool {
+	return func(a, b float64) bool {
+		return Float64Equal(a, b, tolerance)
+	}
+}
