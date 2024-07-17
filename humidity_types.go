d2b7712f9eff5600de90738091dd61c8219fd0f1
Chris Dzombak <chris@dzombak.com>
2024-07-17 10:21:30 -0400

    allow unwrapping RelHumidity to float64

--- humidity_types.go
+++ humidity_types.go
@@ -3,7 +3,8 @@ package libwx
 // RelHumidity represents a relative humidity percentage (0-100, inclusive).
 type RelHumidity int
 
-func (rh RelHumidity) Unwrap() int { return int(rh) }
+func (rh RelHumidity) Unwrap() int            { return int(rh) }
+func (rh RelHumidity) UnwrapFloat64() float64 { return float64(rh) }
 
 // ClampedRelHumidity returns a RelHumidity from the given integer, guaranteed
 // to be within the valid 0-100 (inclusive) range.
