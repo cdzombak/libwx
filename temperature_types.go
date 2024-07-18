39b467f3330f8af6213d12d450041bd684243a06
Chris Dzombak <chris@dzombak.com>
2024-07-18 10:11:37 -0400

    document heat index warning levels

--- temperature_types.go
+++ temperature_types.go
@@ -12,9 +12,14 @@ func (t TempC) Unwrap() float64 { return float64(t) }
 type HeatIndexWarning int
 
 const (
+	// HeatIndexWarningNone indicates the heat index does not warrant elevated caution.
 	HeatIndexWarningNone = iota
+	// HeatIndexWarningCaution indicates fatigue is possible with prolonged exposure and activity. Continuing activity could result in heat cramps.
 	HeatIndexWarningCaution
+	// HeatIndexWarningExtremeCaution indicates heat cramps and heat exhaustion are possible. Continuing activity could result in heat stroke.
 	HeatIndexWarningExtremeCaution
+	// HeatIndexWarningDangerindicates heat cramps and heat exhaustion are likely; heat stroke is probable with continued activity.
 	HeatIndexWarningDanger
+	// HeatIndexWarningExtremeDanger indicates heat stroke is imminent.
 	HeatIndexWarningExtremeDanger
 )
